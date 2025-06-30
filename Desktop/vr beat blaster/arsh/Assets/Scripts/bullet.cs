3866944d337a4f8a3cbe0a33266ad5486176f634
Saket-KP <sk8294370484@gmail.com>
2025-06-30 21:19:59 +0530

    changes pushed

---
+++ Desktop/vr beat blaster/arsh/Assets/Scripts/bullet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Surface") || collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision of bullet with surface detected");
+            Destroy(gameObject);
+        }
+
+        //if (collision.gameObject.CompareTag("EnemySurface"))
+        //{
+        //    Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        //}
+    }
+}
